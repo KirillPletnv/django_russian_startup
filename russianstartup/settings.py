ac1a0f8abeee5e1fcd52c2edd6c97297e0f116b6
Kirill <kirillfromhome@gmail.com>
2025-10-28 20:46:53 +0300

    DEBUG bleat

--- russianstartup/settings.py
+++ russianstartup/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')  # Берет из .env файла
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')  
 
